f2fb282844bde539dd03b31f00e3fdb42c49e0f8
Kettlenet <kettlenet.info@gmail.com>
2025-09-12 12:02:03 +0530

    implementing login/signup

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
+  <NuxtLoadingIndicator color="#FF0000"/>
   <NuxtLayout>
         <NuxtPage />
     </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { NuxtLoadingIndicator } from '#components';
+</script>
+
--- components/Authentication/signup.vue
+++ components/Authentication/signup.vue
@@ -30,15 +30,17 @@
           <div class="row">
             <div class="form-group col-12 col-md-6">
               <label for="inputEmail3">Email Address</label>
-              <input class="form-control" id="inputEmail3" type="email" placeholder="Demo@123gmail.com" v-model="email" />
+              <input class="form-control" id="inputEmail3" type="email" placeholder="Demo@123gmail.com"
+                v-model="email" />
             </div>
             <div class="form-group col-12 col-md-6">
               <label for="inputPassword3">Password</label>
-              <input class="form-control" id="inputPassword3" type="password" placeholder="Password" v-model="password" />
+              <input class="form-control" id="inputPassword3" type="password" placeholder="Password"
+                v-model="password" />
             </div>
           </div>
 
-        
+
 
 
           <!-- Row: DOB + Sexual Orientation -->
@@ -58,118 +60,55 @@
             </div>
           </div>
 
-            <!-- Partner Details (only if Couple) -->
-        <div v-if="profileType === 'Couple'">
-          <h5 class="mt-3 text-white">Partner Details</h5>
-          <div class="row">
-            <div class="form-group col-12 col-md-6">
-              <label for="partnerFirstName">Partner First Name</label>
-              <input
-                class="form-control"
-                id="partnerFirstName"
-                type="text"
-                placeholder="First Name"
-                v-model="partnerFirstName"
-              />
-            </div>
-            <div class="form-group col-12 col-md-6">
-              <label for="partnerLastName">Partner Last Name</label>
-              <input
-                class="form-control"
-                id="partnerLastName"
-                type="text"
-                placeholder="Last Name"
-                v-model="partnerLastName"
-              />
-            </div>
-          </div>
+          <!-- Partner Details (only if Couple) -->
+          <div v-if="profileType === 'Couple'">
+            <h5 class="mt-3 text-white">Partner Details</h5>
 
-          <div class="row">
-            <div class="form-group col-12 col-md-6">
-              <label for="partnerNickName">Partner Nickname</label>
-              <input
-                class="form-control"
-                id="partnerNickName"
-                type="text"
-                placeholder="Nickname"
-                v-model="partnerNickName"
-              />
+            <div class="row">
+              <div class="form-group col-12 col-md-6">
+                <label for="partnerNickName">Partner Nickname</label>
+                <input class="form-control" id="partnerNickName" type="text" placeholder="Nickname"
+                  v-model="partnerNickName" />
+              </div>
+              <div class="form-group col-12 col-md-6">
+                <label for="partnerDob">Partner Date of Birth</label>
+                <input class="form-control" id="partnerDob" type="date" v-model="partnerDob" :max="maxDob" />
+              </div>
             </div>
-            <div class="form-group col-12 col-md-6">
-              <label for="partnerDob">Partner Date of Birth</label>
-              <input
-                class="form-control"
-                id="partnerDob"
-                type="date"
-                v-model="partnerDob"
-                :max="maxDob"
-              />
-            </div>
-          </div>
 
-          <div class="form-group">
-            <label for="partnerSexualOrientation">Partner Sexual Orientation</label>
-            <select
-              class="form-control"
-              id="partnerSexualOrientation"
-              v-model="partnerSexualOrientation"
-            >
-              <option value="">Select Partner Orientation</option>
-              <option
-                v-for="orientation in allOptions.orientations"
-                :key="orientation"
-                :value="orientation"
-              >
-                {{ orientation }}
-              </option>
-            </select>
+            <div class="form-group">
+              <label for="partnerSexualOrientation">Partner Sexual Orientation</label>
+              <select class="form-control" id="partnerSexualOrientation" v-model="partnerSexualOrientation">
+                <option value="">Select Partner Orientation</option>
+                <option v-for="orientation in allOptions.orientations" :key="orientation" :value="orientation">
+                  {{ orientation }}
+                </option>
+              </select>
+            </div>
           </div>
-        </div>
 
           <!-- Sexual Interest (Checkboxes) -->
           <div class="form-group">
             <label>Sexual Interest</label>
-            <Multiselect
-              v-model="sexualInterest"
-              :options="allOptions.interests"
-              track-by="interest_id"
-              label="interest_name"
-              :multiple="true"
-              class="custom-multiselect"
-              :close-on-select="false"
-              placeholder="Select Sexual Interest"
-              :reduce="interestToId"
-            />
+            <Multiselect v-model="sexualInterest" :options="allOptions.interests" track-by="interest_id"
+              label="interest_name" :multiple="true" class="custom-multiselect" :close-on-select="false"
+              placeholder="Select Sexual Interest" :reduce="interestToId" />
           </div>
 
           <!-- Meet Preference (Checkboxes) -->
           <div class="form-group">
             <label>Meet Preference</label>
-            <Multiselect
-              v-model="meetPreference"
-              :options="allOptions.meetPerferences"
-              track-by="meet_preference_id"
-              label="preference_name"
-              :multiple="true"
-              :close-on-select="false"
-              placeholder="Select Meet Preference"
-              :reduce="prefToId"
-            />
+            <Multiselect v-model="meetPreference" :options="allOptions.meetPerferences" track-by="meet_preference_id"
+              label="preference_name" :multiple="true" :close-on-select="false" placeholder="Select Meet Preference"
+              :reduce="prefToId" />
           </div>
 
-              <!-- Meet Preference (Checkboxes) -->
+          <!-- Meet Preference (Checkboxes) -->
           <div class="form-group">
             <label>Looking For</label>
-            <Multiselect
-              v-model="meetPreference"
-              :options="allOptions.meetPerferences"
-              track-by="looking_for_id"
-              label="looking_for_name"
-              :multiple="true"
-              :close-on-select="false"
-              placeholder="Select Looking For"
-              :reduce="lookingForToId"
-            />
+            <Multiselect v-model="meetPreference" :options="allOptions.meetPerferences" track-by="looking_for_id"
+              label="looking_for_name" :multiple="true" :close-on-select="false" placeholder="Select Looking For"
+              :reduce="lookingForToId" />
           </div>
 
           <!-- Signup Button -->
@@ -182,7 +121,7 @@
             </div>
           </div>
 
-           <div class="form-group mb-0">
+          <div class="form-group mb-0">
             <div class="text-center">
               <p class="text-white">Already have an account? <nuxt-link to="/authentication/login">Login</nuxt-link></p>
             </div>
@@ -244,15 +183,14 @@ allOptions.value = await fetchOptions() as UsersModel.FetchOptionsResponseModel
 
 const validateNickname = async () => {
   let nick = nickName.value.trim();
-  if (nick.length === 0) 
-  {
-    return ;
+  if (nick.length === 0) {
+    return;
   }
   try {
     const api_url = getUrl(RequestURL.validateNickname);
     is_nick_loading.value = true;
     is_nick_valid.value = false;
-    
+
     const response = await $fetch<SuccessError<UsersModel.ValidateNicknameResponseModel>>(api_url, {
       method: 'POST',
       body: { nick_name: nickName.value },
@@ -262,64 +200,62 @@ const validateNickname = async () => {
     });
     is_nick_loading.value = false;
     // Handle response as needed
-     is_nick_valid.value = response.success;
-    if (response.success === false)
-    {
-       showalert(response.message)
+    is_nick_valid.value = response.success;
+    if (response.success === false) {
+      showalert(response.message)
     }
 
-   
+
   } catch (error) {
-       is_nick_valid.value = false;
+    is_nick_valid.value = false;
     is_nick_loading.value = false;
     // Handle error as needed
     console.error('Nickname validation failed:', error);
- 
+
   }
 };
 
-function usersignup() 
-{
+function usersignup() {
   if (!checkValidation() || is_signup_loading.value) {
     return;
   }
   let request_mdoel = {
-        profile_type : profileType.value,
-        nick_name : nickName.value,
-        email : email.value,
-        password : password.value,
-        dob : toDate((dob.value ?? ''),'yyyy-MM-dd')?.toISOString().split('T')[0],
-        orientation : sexualOrientation.value,
-        partner_first_name : partnerFirstName.value,
-        partner_last_name : partnerLastName.value,
-        partner_nick_name : partnerNickName.value,
-        partner_orientation : partnerSexualOrientation.value,
-        partner_dob : toDate((partnerDob.value ?? ''),'yyyy-MM-dd')?.toISOString().split('T')[0],
-         interest_ids: sexualInterest.value,
-         meet_preference_ids: meetPreference.value
+    profile_type: profileType.value,
+    nick_name: nickName.value,
+    email: email.value,
+    password: password.value,
+    dob: toDate((dob.value ?? ''), 'yyyy-MM-dd')?.toISOString().split('T')[0],
+    orientation: sexualOrientation.value,
+    partner_first_name: partnerFirstName.value,
+    partner_last_name: partnerLastName.value,
+    partner_nick_name: partnerNickName.value,
+    partner_orientation: partnerSexualOrientation.value,
+    partner_dob: toDate((partnerDob.value ?? ''), 'yyyy-MM-dd')?.toISOString().split('T')[0],
+    interest_ids: sexualInterest.value,
+    meet_preference_ids: meetPreference.value
   } as UsersModel.SignUpRequestModel
 
-    let api_url = getUrl(RequestURL.signup);
-    is_signup_loading.value = true;
-    $fetch<SuccessError<UsersModel.SignUpResponseModel>>(api_url, {
-      method: 'POST',
-      body: request_mdoel,  
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }).then((response) => {
-      is_signup_loading.value = false;
-      if (response.success) {
-        showalert('Signup successful! Please check your email to verify your account.',true);
-        // Optionally, redirect to login page or clear form
-      } else {
-        showalert(response.message || 'Signup failed. Please try again.');
-      }
-    }).catch((error) => {
-      is_signup_loading.value = false;
-      showalert('An error occurred during signup. Please try again.');
-      console.error('Signup failed:', error);
-    });
+  let api_url = getUrl(RequestURL.signup);
+  is_signup_loading.value = true;
+  $fetch<SuccessError<UsersModel.SignUpResponseModel>>(api_url, {
+    method: 'POST',
+    body: request_mdoel,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  }).then((response) => {
+    is_signup_loading.value = false;
+    if (response.success) {
+      showalert('Signup successful! Please check your email to verify your account.', true);
+      // Optionally, redirect to login page or clear form
+    } else {
+      showalert(response.message || 'Signup failed. Please try again.');
+    }
+  }).catch((error) => {
+    is_signup_loading.value = false;
+    showalert('An error occurred during signup. Please try again.');
+    console.error('Signup failed:', error);
+  });
 
   // Proceed with signup logic
 }
@@ -331,8 +267,8 @@ function checkValidation(): boolean {
   const passwordVal = password.value;
   const dobVal = dob.value;
   const sexualorientation = sexualOrientation.value.trim();
-        
-    if (profiletype.length === 0) {
+
+  if (profiletype.length === 0) {
     showalert('Please select profile type');
     return false;
   }
@@ -343,14 +279,13 @@ function checkValidation(): boolean {
   else if (!is_nick_valid.value) {
     showalert('Please enter valid nickname');
     return false;
-  } 
-   else if(profileType.value === 'Couple' && partnerNickName.value.trim().length === 0)
-   {
-      showalert('Please enter partner nickname');
-       return false;
-   }
+  }
+  else if (profileType.value === 'Couple' && partnerNickName.value.trim().length === 0) {
+    showalert('Please enter partner nickname');
+    return false;
+  }
 
-   else if (emailVal.length === 0) {
+  else if (emailVal.length === 0) {
     showalert('Please enter email');
     return false;
   }
@@ -364,32 +299,29 @@ function checkValidation(): boolean {
   }
   else if (dobVal.length === 0) {
     showalert('Please enter date of birth');
-   // toDate((item.created_at ?? ''),'yyyy-MM-dd')?.toISOString().split('T')[0] }}
+    // toDate((item.created_at ?? ''),'yyyy-MM-dd')?.toISOString().split('T')[0] }}
     return false;
   }
- if (profileType.value === 'Couple' && partnerDob.value.length === 0) {
-       showalert('Please enter partner date of birth');
-       return false;
-     }
-   else if (sexualorientation.length === 0)
-    {
+  if (profileType.value === 'Couple' && partnerDob.value.length === 0) {
+    showalert('Please enter partner date of birth');
+    return false;
+  }
+  else if (sexualorientation.length === 0) {
     showalert('Please select sexual orientation');
     return false;
   }
   if (profileType.value === 'Couple' && partnerSexualOrientation.value.trim().length === 0) {
-       showalert('Please select partner sexual orientation');
-       return false;
-     }
-    else if (sexualInterest.value.length === 0)
-    {
-    showalert('Please select sexual interest');
-   // toDate((item.created_at ?? ''),'yyyy-MM-dd')?.toISOString().split('T')[0] }}
+    showalert('Please select partner sexual orientation');
     return false;
   }
-  else if (meetPreference.value.length === 0)
-    {
+  else if (sexualInterest.value.length === 0) {
+    showalert('Please select sexual interest');
+    // toDate((item.created_at ?? ''),'yyyy-MM-dd')?.toISOString().split('T')[0] }}
+    return false;
+  }
+  else if (meetPreference.value.length === 0) {
     showalert('Please select meet preference');
-   // toDate((item.created_at ?? ''),'yyyy-MM-dd')?.toISOString().split('T')[0] }}
+    // toDate((item.created_at ?? ''),'yyyy-MM-dd')?.toISOString().split('T')[0] }}
     return false;
   }
   // Add more checks as needed (e.g., sexual orientation, interests, meet preference)
