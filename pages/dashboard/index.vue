933c99fa073b8f8ebf22e5de0cd060c3bddd67e3
adarshsharma11 <adarshsharma002@gmail.com>
2025-10-04 16:54:34 +0530

    dashboard page ui

---
+++ pages/dashboard/index.vue
@@ -0,0 +1,11 @@
+<template>
+   <ProfileUserDashboard/>
+</template>
+<script setup>  
+useHead({
+    title: "LinkSwinger – Discover Like-Minded Connections"
+})
+ definePageMeta({
+        middleware:'auth'
+    })
+</script>
