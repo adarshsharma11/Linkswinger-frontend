9f3e55cc59d1a1ab35147dbf8aa289a11c1477e3
KettleNet Technologies <kettlenet.info@gmail.com>
2025-10-10 17:01:25 +0530

    implementing promotional text

--- app.vue
+++ app.vue
@@ -1,13 +1,32 @@
 <template>
-    <NuxtLoadingIndicator color="#FF0000" />
+  <NuxtLoadingIndicator color="#FF0000" />
+  <CommonPromotionStrip :text="promotion_text" />
+  <div class="mb-6">
     <NuxtLayout>
-        <NuxtPage />
+      <NuxtPage />
     </NuxtLayout>
+  </div>
 </template>
 
 <script setup lang="ts">
 import { NuxtLoadingIndicator } from '#components';
+import type { PromotionsModel } from './composables/models';
 const id_store = idStore()
+const promotion_text = ref('')
+
+const fetchOptions = async () => {
+  const api_url = getUrl(RequestURL.fetchPromotion);
+  const { data: fetch_response, error: option_error } = await useFetch<SuccessError<PromotionsModel.FetchResponseModel>>(api_url, {
+    cache: "no-cache",
+    method: "post",
+    body: {},
+    headers: {
+      "content-type": "application/json"
+    }
+  });
+  return fetch_response.value?.response?.text ?? ''
+}
+promotion_text.value = await fetchOptions()
 
 onMounted(() => {
     useDatabase();
@@ -21,3 +40,9 @@ onMounted(() => {
 });
 
 </script>
+<style scoped>
+/* Or Tailwind class works just fine */
+.mb-6 {
+  margin-top: 50px; /* adjust as needed */
+}
+</style>
