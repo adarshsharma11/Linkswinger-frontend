296dd857f0f2c317b71a5fd3515327663f30fe29
KettleNet Technologies <kettlenet.info@gmail.com>
2025-10-13 14:54:04 +0530

    added gtag

--- nuxt.config.js
+++ nuxt.config.js
@@ -10,7 +10,17 @@ export default defineNuxtConfig({
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Montserrat:400,500,600,700,800&amp;display=swap' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Roboto:400,500,600&amp;display=swap' },
         { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Rubik:300,300i,400,400i,500,500i,700,700i,900,900i&amp;display=swap' }
-      ]
+      ],
+      script: [
+        { src: 'https://www.googletagmanager.com/gtag/js?id=AW-17646009228', async: true },
+        { children: "window.dataLayer = window.dataLayer || []; function gtag(){dataLayer.push(arguments);} gtag('js', new Date()); gtag('config', 'AW-17646009228');", type: 'text/javascript', charset: 'utf-8' }
+      ],
+      // meta: [
+      //   { charset: 'utf-8' },
+      //   { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      //   { hid: 'description', name: 'description', content: 'LinkSwinger is a free link management tool that allows you to shorten, manage, and track your links with ease. Perfect for marketers, businesses, and anyone looking to optimize their online presence.' },
+      //   { name: 'format-detection', content: 'telephone=no' }
+      // ]
     },
     baseURL: "/"
   },
