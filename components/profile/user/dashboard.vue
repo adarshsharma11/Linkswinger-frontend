d66924878616e21c92c226d874f554607ee15fcc
KettleNet Technologies <kettlenet.info@gmail.com>
2025-11-07 12:06:48 +0530

    implementing call

--- components/profile/user/dashboard.vue
+++ components/profile/user/dashboard.vue
@@ -563,13 +563,6 @@
     </div>
   </div>
 
-
-
-
-
-
-
-
   <div class="modal fade ad-search-modal" id="advancesearchmodal" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered modal-lg modal-dialog-scrollable" role="document">
       <div class="modal-content bg-dark text-white">
