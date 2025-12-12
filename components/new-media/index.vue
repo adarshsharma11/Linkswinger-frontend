7154b1b00af5dc6a0b2b2ef70adf19376d948c54
adarshsharma11 <adarshsharma002@gmail.com>
2025-12-12 19:10:57 +0530

    new media page ui

---
+++ components/new-media/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NewMediaContent from './new-media-content.vue';
+
+</script>
+
+<template>
+    <div class="theme-landing">
+        <NewMediaContent/>
+        <CommonFooter/>
+        </div>
+</template>
---
+++ pages/new-media/index.vue
@@ -0,0 +1,11 @@
+<template>
+   <new-media/>
+</template>
+<script setup>
+useHead({
+    title: "LinkSwinger – Discover Like-Minded Connections"
+})
+ definePageMeta({
+        middleware:'auth'
+    })
+</script>
