61baedff483fff97fdcad5551de8695c05abf467
KettleNet Technologies <kettlenet.info@gmail.com>
2025-12-13 12:25:02 +0530

    new media model issue

--- components/messenger_page/index.vue
+++ components/messenger_page/index.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div>
+  <!-- <div>
     <client-only>
       <div class="chitchat-container sidebar-toggle">
         <MessengerPageChat/>
@@ -22,70 +22,70 @@
 <MessengerPageCustomizer/>
      
     </client-only>
-  </div>
+  </div> -->
   
 </template>
 
 <script setup>
-import { ref, onMounted, onUnmounted, watchEffect } from "vue";
-import layout from "@/data/layout.json";
-import { useCommonStore } from "@/store/common";
-import { MessengerPageModalsConfigurationmodel, MessengerPageModalsCreatetodo, MessengerPageModalsSetreminder } from "#components";
+// import { ref, onMounted, onUnmounted, watchEffect } from "vue";
+// import layout from "@/data/layout.json";
+// import { useCommonStore } from "@/store/common";
+// import { MessengerPageModalsConfigurationmodel, MessengerPageModalsCreatetodo, MessengerPageModalsSetreminder } from "#components";
 
 
-const store = useCommonStore();
+// const store = useCommonStore();
 
 
-const width = ref(0);
+// const width = ref(0);
 
 
-const handleResize = () => {
-  if (process.client) {
-    width.value = window.innerWidth;
-  }
-};
+// const handleResize = () => {
+//   if (process.client) {
+//     width.value = window.innerWidth;
+//   }
+// };
 
 
-watchEffect(() => {
-  if (width.value < 992) {
-    store.toggleleftside = "";
-  } else if (width.value > 992 && layout.config.sidebar_setting) {
-    store.toggleleftside = "on";
-  } else {
-    store.toggleleftside = "";
-  }
-});
+// watchEffect(() => {
+//   if (width.value < 992) {
+//     store.toggleleftside = "";
+//   } else if (width.value > 992 && layout.config.sidebar_setting) {
+//     store.toggleleftside = "on";
+//   } else {
+//     store.toggleleftside = "";
+//   }
+// });
 
 
-onMounted(() => {
-  window.addEventListener("resize", handleResize);
-  handleResize();
+// onMounted(() => {
+//   window.addEventListener("resize", handleResize);
+//   handleResize();
 
-  if (width.value < 992) {
-    store.toggleleftside = "";
-  } else if (width.value > 992 && layout.config.sidebar_setting) {
-    store.toggleleftside = "on";
-  } else {
-    store.toggleleftside = "";
-  }
+//   if (width.value < 992) {
+//     store.toggleleftside = "";
+//   } else if (width.value > 992 && layout.config.sidebar_setting) {
+//     store.toggleleftside = "on";
+//   } else {
+//     store.toggleleftside = "";
+//   }
 
-  store.activechat = true;
-  store.activesidebar = 0;
-  store.activechatType = 1;
-});
+//   store.activechat = true;
+//   store.activesidebar = 0;
+//   store.activechatType = 1;
+// });
 
-onUnmounted(() => {
-  window.removeEventListener("resize", handleResize);
-});
+// onUnmounted(() => {
+//   window.removeEventListener("resize", handleResize);
+// });
 
 
-useHead({
-  bodyAttrs: {
-    class: `main-page ${layout.config.sidebar_layout}`,
-    id: "body",
-  },
-  htmlAttrs: {
-    class: `${layout.config.color}`,
-  },
-});
+// useHead({
+//   bodyAttrs: {
+//     class: `main-page ${layout.config.sidebar_layout}`,
+//     id: "body",
+//   },
+//   htmlAttrs: {
+//     class: `${layout.config.color}`,
+//   },
+// });
 </script>
