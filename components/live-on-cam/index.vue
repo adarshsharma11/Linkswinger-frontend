581c552db0fbbad0c1467c4de53dfd7402933f8a
adarshsharma11 <adarshsharma002@gmail.com>
2025-12-16 17:11:09 +0530

    live cam & meet events page ui

---
+++ components/live-on-cam/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import LiveOnCamContent from './live-on-cam-content.vue';
+
+</script>
+
+<template>
+    <div class="theme-landing">
+        <LiveOnCamContent/>
+        <CommonFooter/>
+        </div>
+</template>
---
+++ components/meet-events/index.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import MeetEventsContent from './meet-events-content.vue';
+
+</script>
+
+<template>
+    <div class="theme-landing">
+        <MeetEventsContent/>
+        <CommonFooter/>
+        </div>
+</template>
