9c6123de579a1d34bda44351cb965dfc6432509d
KettleNet Technologies <kettlenet.info@gmail.com>
2025-12-24 13:10:07 +0530

    implemented single player

--- components/profile/user/dashboard.vue
+++ components/profile/user/dashboard.vue
@@ -1222,7 +1222,7 @@ onMounted(() => {
   else if (hash === '#friends') {
     fetchFriends()
   }
-  else if (hash === '#home') {
+  else if (hash === '#home' || activeNav.value === 'home') {
     fetchHome()
   }
 
